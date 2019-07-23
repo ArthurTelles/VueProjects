50dbded663a031e6a4608e7f86d373d15905ed98
Arthur <atelles96@gmail.com>
2019-07-23 19:18:34 -0300

    Add: Data files cats.js and dogs.js.
    Added to data files on the data folder.

--- adopt/src/views/Cats.vue
+++ adopt/src/views/Cats.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        Cats
-    </div>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
