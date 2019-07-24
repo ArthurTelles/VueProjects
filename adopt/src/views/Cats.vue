47f4965eb0b1b110fc3bbf05a15efab13170ebd2
Unknown <atelles96@gmail.com>
2019-07-24 01:54:51 -0300

    Feat: New boards added to screens using bootstrap.

--- adopt/src/views/Cats.vue
+++ adopt/src/views/Cats.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <b-table striped hover :items="items"></b-table>
+      <h1>Cats for adoption</h1>
+    <b-table striped hover :items="cats"></b-table>
   </div>
 </template>
 
 <script>
+import cats from '@/data/cats'
+
 export default {
     data() {
         return {
-            
+            cats
         }
     }
 }
