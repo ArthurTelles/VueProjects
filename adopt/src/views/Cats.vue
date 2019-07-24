4c5a15f51eced8ee247e2ced433cf4cc28d8cfed
Arthur <atelles96@gmail.com>
2019-07-24 18:12:43 -0300

    Feat: New store folder configuration.

--- adopt/src/views/Cats.vue
+++ adopt/src/views/Cats.vue
@@ -1,18 +1,32 @@
 <template>
+  
   <div>
-      <h1>Cats for adoption</h1>
-    <b-table striped hover :items="cats"></b-table>
+
+    <h1>Cats for adoption</h1>
+
+    <b-table striped hover :items="cats">
+      <template slot="name" slot-scope="data">
+        <router-link :to="`/pets/cats/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
+
   </div>
+
 </template>
 
 <script>
-import cats from '@/data/cats'
+import { mapState } from "vuex"
 
 export default {
-    data() {
-        return {
-            cats
-        }
-    }
-}
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+};
 </script>
--- adopt/src/views/Dogs.vue
+++ adopt/src/views/Dogs.vue
@@ -1,18 +1,30 @@
 <template>
-    <div>
-        <h1>Dogs for adoption</h1>
-        <b-table striped hover :items="dogs" />
-    </div>
+
+  <div>
+
+    <h1>Dogs for adoption</h1>
+
+    <b-table striped hover :items="dogs">
+      <template slot="name" slot-scope="data">
+        <router-link :to="`/pets/dogs/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
+
+  </div>
+
 </template>
 
 <script>
-import dogs from '@/data/dogs'
+import { mapState } from "vuex";
 
 export default {
-    data() {
-        return {
-            dogs
-        }
-    }
-}
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["dogs"])
+  }
+};
 </script>
